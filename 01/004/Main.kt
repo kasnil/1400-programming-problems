c67222f493d636e0136b835f26d02f0b0e3eb018
kasnil <kasnil@outlook.ie>
2025-07-18 20:29:31 +0300

    01/004

---
+++ 01/004/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    solve()
+}
+
+/**
+ * Вывести на экран числа 5, 10 и 21 одно под другим.
+ */
+fun solve() {
+    println(intArrayOf(5, 10, 21).joinToString(separator = "\r\n"))
+}
