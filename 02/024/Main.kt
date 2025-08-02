47119ba97a8ca0f671a3ba78246536e78a57e32b
kasnil <kasnil@outlook.ie>
2025-08-02 09:15:15 +0300

    02/024

---
+++ 02/024/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    solve()
+}
+
+/**
+ * Даны два числа. Найти их сумму, разность, произведение, а также частное
+ * от деления первого числа на второе.
+ */
+fun solve() {
+    print("Введите первое число: ")
+    val a = readLine()?.trim()?.toDoubleOrNull() ?: 0.0
+    print("Введите второе число: ")
+    val b = readLine()?.trim()?.toDoubleOrNull() ?: 0.0
+    val c = a + b
+    val d = a - b
+    val e = a * b
+    val f = a / b
+    println("Сумма чисел: $c")
+    println("Разность: $d")
+    println("Произведение: $e")
+    println("Частное: $f")
+}
